9c755721fd1a8d143dbaf424420c791fc17e2d24
Evan Chen <evanwchen@gmail.com>
2015-12-06 21:53:11 -0800

    Complete stringifyJSON
    Wrote stringifyJSON function to emulate the JSON.stringify function.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,40 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // object
+  if (Object.prototype.toString.call(obj) === "[object Object]") {
+  	var temp = [];
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        if (stringifyJSON(obj[prop]) != null && prop != 'undefined') {
+        	temp.push(stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]));
+        }
+			}
+    }
+    return '{' + temp.join(',') + '}';
+  }
+  // array
+  else if (Array.isArray(obj)) {
+  	var strArray = "[";
+  	for (var i=0; i<obj.length; i++) {
+  		strArray += (i ? "," : "") + stringifyJSON(obj[i]);
+  	}
+  	return strArray + ']';
+  }
+  // String
+  else if (typeof obj === "string") { return '"' + obj + '"'; }
+  // Number
+  else if (typeof obj === "number") { return String(obj); }
+  // Boolean
+  else if (typeof obj === "boolean") { return String(obj); }
+  // null
+  else if (obj == null) { return 'null'; }
+  // function
+  else if (typeof obj === "function") { return; }
+  // symbol
+  else if (typeof obj === "symbol") { return; }
+  // non-enumberable properties
+  else if (typeof obj === "undefined") { return; }
+  //else
+  else { return; }
 };
