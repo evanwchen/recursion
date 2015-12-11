531c2e04049ac198c4892ab415e24712d5e011c0
Evan Chen <evanwchen@gmail.com>
2015-12-10 21:24:32 -0800

    Complete getElementsByClassName.js
    Write getElementsByClassName function to match document.getElementsByClassName().

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
-};
+var getElementsByClassName = function(className){
+  var elements = []
+  var nodes = document.children;
+  for (var i = 0; i < nodes.length; i++) {
+    recurse(className, nodes[i]);
+  }
+  return elements;
+  
+  function recurse(className, node) {
+    checkElement(className, node);
+
+    if (node.children.length > 0) { 
+      var childs = node.children;
+      for (var j = 0; j < childs.length; j++) {
+        recurse(className, childs[j]);
+      } 
+    } 
+  }
+
+  function checkElement(className, node) {
+    if (node.classList != undefined && node.classList.contains(className)) {
+      elements.push(node);
+    }    
+  }
+}
